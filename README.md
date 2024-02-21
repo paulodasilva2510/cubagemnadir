33c9a93cde21365f466f042cd5ddabef40f2db1a
Paulo Cesar da Silva <138016190+paulodasilva2510@users.noreply.github.com>
2024-02-21 10:15:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cubagemnadir
+ Nadir Expedição
---
+++ index.js
@@ -0,0 +1,35 @@
+document
+  .getElementById('calculadora')
+  .addEventListener('submit', function (evento) {
+    evento.preventDefault(); // Previne o comportamento padrão de envio do formulário
+
+    // Obtém os valores do formulário
+    var comprimentoCaminhao = parseFloat(
+      document.getElementById('comprimentoCaminhao').value
+    );
+    var larguraCaminhao = parseFloat(
+      document.getElementById('larguraCaminhao').value
+    );
+    var volumeCarga = parseFloat(document.getElementById('volumeCarga').value);
+
+    // Verifica se os valores são números
+    if (
+      isNaN(comprimentoCaminhao) ||
+      isNaN(larguraCaminhao) ||
+      isNaN(volumeCarga)
+    ) {
+      document.getElementById('resultado').textContent =
+        'Por favor, insira números válidos.';
+      return;
+    }
+
+    // Calcula a altura da carga
+    var alturaCarga = volumeCarga / comprimentoCaminhao / larguraCaminhao;
+
+    // Adiciona uma margem de 10%
+    var alturaCargaComMargem = alturaCarga * 1.1;
+
+    // Exibe o resultado
+    document.getElementById('resultado').textContent =
+      'Altura da Carga: ' + alturaCargaComMargem.toFixed(2) + ' m';
+  });
